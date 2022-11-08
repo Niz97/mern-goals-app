ef03dbb4ceac4304b23fe81becb42fac38c38e5b
Niz97 <97niran@gmail.com>
2022-11-08 20:29:09 +0000

    login and out functionality completed. Logged in users not able to access registered endpoint

--- frontend/src/features/auth/authService.js
+++ frontend/src/features/auth/authService.js
@@ -1,27 +1,37 @@
+import axios from 'axios'
 const API_URL = '/api/users/'
 
 // Register user
 const register = async (userData) => {
-  const response = await fetch(API_URL, {
-    method: 'POST',
-    headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(userData)
-  })
-  
-  const result = await response.json()
+  const response = await axios.post(API_URL, userData)
 
-  if (result.data) {
-    localStorage.setItem('user', JSON.stringify(result.data))
+  if (response.data) {
+    localStorage.setItem('user', JSON.stringify(response.data))
   }
 
-  return result.data
+  return response.data
+}
+
+// Login user
+const login = async (userData) => {
+  const response = await axios.post(`API_URL${login}`, userData)
+
+  if (response.data) {
+    localStorage.setItem('user', JSON.stringify(response.data))
+  }
+
+  return response.data
+}
+
+// Logout user
+const logout = () => {
+  localStorage.removeItem('user')
 }
 
 const authService = {
-  register
+  register,
+  logout,
+  login,
 }
 
 export default authService
